6a8029d67a72284dd1b0852c8c07ac4345f79af6
Jay150703 <jayanthijemi15@gmail.com>
2026-03-02 10:38:32 +0530

    Initial Maven project setup with JUnit configuration

---
+++ src/test/java/com/jay/SanityTest.java
@@ -0,0 +1,12 @@
+package com.jay;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SanityTest {
+
+    @Test
+    void testBasicMath() {
+        assertEquals(2, 1 + 1);
+    }
+}
